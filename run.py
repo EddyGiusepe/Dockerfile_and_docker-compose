76005bfedb32305e48eef0950aebc6805540d860
EddyGiusepe <eddychirinos.unac@gmail.com>
2022-11-11 19:40:25 -0300

    Arquivos para testar

--- run.py
+++ run.py
@@ -1,12 +1,23 @@
 '''
 Data Scientist.: Dr.Eddy Giusepe Chirinos Isidro
 '''
-from flask import Flask
+from src import UserRepo
+from flask import Flask, request
 
 app = Flask(__name__)
-
 
 @app.route("/", methods=["GET"])
 def hello_world():
     '''Função de teste apenas'''
     return "Olá, estou na aplicação setada!"
+
+@app.route("/insert", methods=["POST"])
+def insert():
+    '''Função de insert'''
+    userRepo = UserRepo()
+    body = request.json
+
+    userRepo.insert_user(body["name"])
+
+    return 'ok'
+
